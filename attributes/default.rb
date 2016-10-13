d6b228c044d73198dac91d0107a38197c57ba962
Trevor <trevor.foster@hotmail.ca>
2016-10-13 02:41:49 -0400

    -Added node attribute for installation of td-agent gems

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default["td_agent"]["api_key"] = ''
 
 default["td_agent"]["plugins"] = []
+default["td_agent"]["gems"] = []
 
 default["td_agent"]["uid"] = nil
 default["td_agent"]["gid"] = nil
@@ -16,11 +17,11 @@ default["td_agent"]["version"] = "2.2.0"
 default["td_agent"]["pinning_version"] = false
 default["td_agent"]["apt_arch"] = 'amd64'
 default["td_agent"]["in_forward"] = {
-  port: 24224,
-  bind: '0.0.0.0'
+    port: 24224,
+    bind: '0.0.0.0'
 }
 default["td_agent"]["in_http"] = {
-  port: 8888,
-  bind: '0.0.0.0'
+    port: 8888,
+    bind: '0.0.0.0'
 }
 default["td_agent"]["yum_amazon_releasever"] = "$releasever"
--- recipes/default.rb
+++ recipes/default.rb
@@ -12,112 +12,112 @@ group node["td_agent"]["group"] do
   group_name node["td_agent"]["group"]
   gid node["td_agent"]["gid"] if node["td_agent"]["gid"]
   system true
-  action     [:create]
+  action [:create]
 end
 
 user node["td_agent"]["user"] do
-  comment  'td-agent'
+  comment 'td-agent'
   uid node["td_agent"]["uid"] if node["td_agent"]["uid"]
   system true
-  group    node["td_agent"]["group"]
-  home     '/var/run/td-agent'
-  shell    '/bin/false'
+  group node["td_agent"]["group"]
+  home '/var/run/td-agent'
+  shell '/bin/false'
   password nil
   supports :manage_home => true
-  action   [:create, :manage]
+  action [:create, :manage]
 end
 
 directory '/var/run/td-agent/' do
-  owner  node["td_agent"]["user"]
-  group  node["td_agent"]["group"]
-  mode   '0755'
+  owner node["td_agent"]["user"]
+  group node["td_agent"]["group"]
+  mode '0755'
   action :create
 end
 
 directory '/etc/td-agent/' do
-  owner  node["td_agent"]["user"]
-  group  node["td_agent"]["group"]
-  mode   '0755'
+  owner node["td_agent"]["user"]
+  group node["td_agent"]["group"]
+  mode '0755'
   action :create
 end
 
 case node['platform_family']
-when "debian"
-  dist = node['lsb']['codename']
-  platform = node["platform"]
-  source =
-    if major.nil? || major == '1'
-      # version 1.x or no version
-      if dist == 'precise'
-        'http://packages.treasuredata.com/precise/'
-      else
-        'http://packages.treasuredata.com/debian/'
-      end
-    else
-      # version 2.x or later
-      "http://packages.treasuredata.com/#{major}/#{platform}/#{dist}/"
-    end
-
-  apt_repository "treasure-data" do
-    uri source
-    arch node["td_agent"]["apt_arch"]
-    distribution dist
-    components ["contrib"]
-    key "https://packages.treasuredata.com/GPG-KEY-td-agent"
-    action :add
-  end
-when "fedora"
-  platform = node["platform"]
-  source =
-    if major.nil? || major == '1'
-      "http://packages.treasuredata.com/redhat/$basearch"
-    else
-      # version 2.x or later
-        "http://packages.treasuredata.com/2/redhat/7/$basearch"
-    end
-
-  yum_repository "treasure-data" do
-    description "TreasureData"
-    url source
-    gpgkey "https://packages.treasuredata.com/GPG-KEY-td-agent"
-    action :add
-  end
-when "rhel"
-  platform = node["platform"]
-  source =
-    if major.nil? || major == '1'
-      "http://packages.treasuredata.com/redhat/$basearch"
-    else
-      # version 2.x or later
-      if platform == "amazon"
-        if node["td_agent"]["yum_amazon_releasever"] != "$releasever"
-          Chef::Log.warn("Treasure Data doesn't guarantee td-agent works on older Amazon Linux releases. td-agent could be used on such environment at your own risk.")
+  when "debian"
+    dist = node['lsb']['codename']
+    platform = node["platform"]
+    source =
+        if major.nil? || major == '1'
+          # version 1.x or no version
+          if dist == 'precise'
+            'http://packages.treasuredata.com/precise/'
+          else
+            'http://packages.treasuredata.com/debian/'
+          end
+        else
+          # version 2.x or later
+          "http://packages.treasuredata.com/#{major}/#{platform}/#{dist}/"
         end
-        "http://packages.treasuredata.com/2/redhat/#{node["td_agent"]["yum_amazon_releasever"]}/$basearch"
-      else
-        "http://packages.treasuredata.com/2/redhat/$releasever/$basearch"
-      end
-    end
 
-  yum_repository "treasure-data" do
-    description "TreasureData"
-    url source
-    gpgkey "https://packages.treasuredata.com/GPG-KEY-td-agent"
-    action :add
-  end
+    apt_repository "treasure-data" do
+      uri source
+      arch node["td_agent"]["apt_arch"]
+      distribution dist
+      components ["contrib"]
+      key "https://packages.treasuredata.com/GPG-KEY-td-agent"
+      action :add
+    end
+  when "fedora"
+    platform = node["platform"]
+    source =
+        if major.nil? || major == '1'
+          "http://packages.treasuredata.com/redhat/$basearch"
+        else
+          # version 2.x or later
+          "http://packages.treasuredata.com/2/redhat/7/$basearch"
+        end
+
+    yum_repository "treasure-data" do
+      description "TreasureData"
+      url source
+      gpgkey "https://packages.treasuredata.com/GPG-KEY-td-agent"
+      action :add
+    end
+  when "rhel"
+    platform = node["platform"]
+    source =
+        if major.nil? || major == '1'
+          "http://packages.treasuredata.com/redhat/$basearch"
+        else
+          # version 2.x or later
+          if platform == "amazon"
+            if node["td_agent"]["yum_amazon_releasever"] != "$releasever"
+              Chef::Log.warn("Treasure Data doesn't guarantee td-agent works on older Amazon Linux releases. td-agent could be used on such environment at your own risk.")
+            end
+            "http://packages.treasuredata.com/2/redhat/#{node["td_agent"]["yum_amazon_releasever"]}/$basearch"
+          else
+            "http://packages.treasuredata.com/2/redhat/$releasever/$basearch"
+          end
+        end
+
+    yum_repository "treasure-data" do
+      description "TreasureData"
+      url source
+      gpgkey "https://packages.treasuredata.com/GPG-KEY-td-agent"
+      action :add
+    end
 end
 
 reload_action = (reload_available?) ? :reload : :restart
 
 major_version = major
 template "/etc/td-agent/td-agent.conf" do
-  owner  node["td_agent"]["user"]
-  group  node["td_agent"]["group"]
+  owner node["td_agent"]["user"]
+  group node["td_agent"]["group"]
   mode "0644"
   cookbook node['td_agent']['template_cookbook']
   source "td-agent.conf.erb"
   variables(
-    :major_version => major_version
+      :major_version => major_version
   )
   notifies reload_action, "service[td-agent]", :delayed
 end
@@ -140,6 +140,14 @@ package "td-agent" do
   end
 end
 
+node["td_agent"]["gems"].each do |gem|
+  if plugin.is_a?(String)
+    td_agent_gem gem do
+      plugin false
+    end
+  end
+end
+
 node["td_agent"]["plugins"].each do |plugin|
   if plugin.is_a?(Hash)
     plugin_name, plugin_attributes = plugin.first
@@ -158,7 +166,7 @@ end
 
 service "td-agent" do
   supports :restart => true, :reload => (reload_action == :reload), :status => true
-  action [ :enable, :start ]
+  action [:enable, :start]
 end
 
 
